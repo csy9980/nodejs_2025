98f6edf64e93f2e0a6abfe941ff4877df69f4b1f
csy9980 <csy9980@gmail.com>
2025-06-18 11:11:47 +0900

    new file:   03/ch03_01.js 	new file:   03/modules/math.js

---
+++ 03/ch03_01.js
@@ -0,0 +1,5 @@
+// common.js 모듈 임포트 방식
+const math = require("./modules/math");
+
+console.log(`math.add(5, 3) => ${math.add(5, 3)}`);
+console.log(`math.substract(10, 4) => ${math.subtract(10, 4)}`);
